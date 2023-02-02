343efce882c2f602fb1dc91067b4beafc6fca589
Coder Saiful <md.syful.max970@gmail.com>
2023-02-02 03:25:36 -0600

    add cors package

--- middlewares/index.js
+++ middlewares/index.js
@@ -6,7 +6,9 @@ const compression = require('compression');
 module.exports = (app) => {
     app.use(cors({
         origin: [process.env.LOCAL_URL, process.env.FRONTEND_URL],
-        methods: ["GET", "POST", "PUT", "DELETE"]
+        methods: ["GET", "POST", "PUT", "DELETE"],
+        credentials: true,
+        optionsSuccessStatus: 200
     }));
     app.use(compression());
     app.use(express.json());
