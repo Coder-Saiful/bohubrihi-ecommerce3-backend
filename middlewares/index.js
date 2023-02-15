7a4cb0c600d9d44e98b358ef4c8994f4238a023e
Coder Saiful <md.syful.max970@gmail.com>
2023-02-14 21:33:59 -0600

    s

--- middlewares/index.js
+++ middlewares/index.js
@@ -4,12 +4,7 @@ const morgan = require('morgan');
 const compression = require('compression');
 
 module.exports = (app) => {
-    app.use(cors({
-        origin: [process.env.LOCAL_URL, process.env.FRONTEND_URL],
-        methods: ["GET", "POST", "PUT", "DELETE"],
-        credentials: true,
-        optionsSuccessStatus: 200
-    }));
+    app.use(cors());
     app.use(compression());
     app.use(express.json());
     if (process.env.NODE_ENV === "development") {
