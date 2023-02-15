1a6934b26d038aa3c823692f77a4b3cd2e588223
Coder Saiful <md.syful.max970@gmail.com>
2023-02-14 19:40:13 -0600

    set header

--- middlewares/routes.js
+++ middlewares/routes.js
@@ -5,7 +5,14 @@ const cartRouter = require('../routers/cartRouter');
 const profileRouter = require('../routers/profileRouter');
 
 module.exports = (app) => {
-    app.get('/', (req, res) => res.send('Server running...'));
+    app.get('/', (req, res) => {
+        res.setHeader("Access-Control-Allow-Origin", "*")
+        res.setHeader("Access-Control-Allow-Credentials", "true");
+        res.setHeader("Access-Control-Max-Age", "1800");
+        res.setHeader("Access-Control-Allow-Headers", "content-type");
+        res.setHeader( "Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, PATCH, OPTIONS" ); 
+        res.send('Server running...');
+    });
     app.use('/api/user', userRouter);
     app.use('/api/category', categoryRouter);
     app.use('/api/product', productRouter);
