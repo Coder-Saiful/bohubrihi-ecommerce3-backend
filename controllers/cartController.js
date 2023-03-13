b10eef720249c08b396b55dd1696453dcd406403
Coder Saiful <md.syful.max970@gmail.com>
2023-03-13 11:24:21 -0600

    Error handling in cartController.js

--- controllers/cartController.js
+++ controllers/cartController.js
@@ -3,51 +3,71 @@ const _ = require('lodash');
 
 // add to cart 
 module.exports.createCart = async (req, res) => {
-    const {price, product} = _.pick(req.body, ["price", "product"]);
-    const userId = req.user._id;
-    let cartItem = await Cart.findOne({
-        user: userId,
-        product: product
-    });
-
-    if (cartItem) {
-        return res.status(400).send({message: "Cart item already exists in cart!"});
-    } else {
-        cartItem = new Cart({product: product, price: price, user: userId});
-        const result = await cartItem.save();
-        return res.status(201).send({
-            message: "Added to cart successfully!",
-            data: result
+    try {
+        const {price, product} = _.pick(req.body, ["price", "product"]);
+        const userId = req.user._id;
+        let cartItem = await Cart.findOne({
+            user: userId,
+            product: product
         });
+
+        if (cartItem) {
+            return res.status(400).send({message: "Cart item already exists in cart!"});
+        } else {
+            cartItem = new Cart({product: product, price: price, user: userId});
+            const result = await cartItem.save();
+            return res.status(201).send({
+                message: "Added to cart successfully!",
+                data: result
+            });
+        }
+    } catch (error) {
+        return res.status(400).send({message: "Added to cart failed!"});
     }
 }
 
 // get all the cart item
 module.exports.getCart = async (req, res) => {
-    const userId = req.user._id;
-    const cartItems = await Cart.find({
-        user: userId
-    })
-        .populate('product', 'name')
-        .populate('user', 'name');
-    return res.status(200).send(cartItems);
+    try {
+        const userId = req.user._id;
+        const cartItems = await Cart.find({
+            user: userId
+        })
+            .populate('product', 'name')
+            .populate('user', 'name');
+        if (cartItems.length > 0) {
+            return res.status(200).send(cartItems);
+        } else {
+            return res.status(400).send({message: "There are no items in this cart."});
+        }
+    } catch (error) {
+        return res.status(400).send({message: "Failed to load cart items."});
+    }
 }
 
 // cart item update
 module.exports.updateCart = async (req, res) => {
-    const {_id, count} = _.pick(req.body, ["_id", "count"]);
-    const userId = req.user._id;
-    await Cart.updateOne({ user: userId, _id: _id }, {count: count});
-    return res.status(200).send({message: "Your cart item updated!"});
+    try {
+        const {_id, count} = _.pick(req.body, ["_id", "count"]);
+        const userId = req.user._id;
+        await Cart.updateOne({ user: userId, _id: _id }, {count: count});
+        return res.status(200).send({message: "Your cart item updated successfully."});
+    } catch (error) {
+        return res.status(400).send({message: "Your cart item updated failed."});
+    }
 }
 
 // cart item delete
 module.exports.deleteCart = async (req, res) => {
-    const cartItemId = req.params.id;
-    const userId = req.user._id;
-    await Cart.deleteOne({
-        user:userId,
-        _id: cartItemId
-    });
-    return res.status(200).send({message: "Your cart item removed!"});
+    try {
+        const cartItemId = req.params.id;
+        const userId = req.user._id;
+        await Cart.deleteOne({
+            user:userId,
+            _id: cartItemId
+        });
+        return res.status(200).send({message: "Your cart item removed successfully."});
+    } catch (error) {
+        return res.status(400).send({message: "Your cart item removed failed."});
+    }
 }
--- controllers/profileController.js
+++ controllers/profileController.js
@@ -3,21 +3,29 @@ const _ = require('lodash');
 
 // get profile info
 module.exports.getProfile = async (req, res) => {
-    const prifle = await Profile.findOne({user: req.user._id});
-    return res.status(200).send(prifle);
+    try {
+        const prifle = await Profile.findOne({user: req.user._id});
+        return res.status(200).send(prifle);
+    } catch (error) {
+        return res.status(400).send({message: "An error occured."});
+    }
 }
 
 // set profile info
 module.exports.setProfile = async (req, res) => {
-    const userProfile = _.pick(req.body, ["phone", "address1", "address2", "city", "state", "postcode", "country"]);
-    userProfile.user = req.user._id;
-    let profile = await Profile.findOne({user: req.user._id});
-    if (profile) {
-        await Profile.updateOne({user: req.body._id}, userProfile);
-        return res.status(200).send({message: "Your profile updated successfully!"});
-    } else {
-        profile = new Profile(userProfile);
-        await Profile.save();
-        return res.status(200).send({message: "Your profile info saved successfully!"});
+    try {
+        const userProfile = _.pick(req.body, ["phone", "address1", "address2", "city", "state", "postcode", "country"]);
+        userProfile.user = req.user._id;
+        let profile = await Profile.findOne({user: req.user._id});
+        if (profile) {
+            await Profile.updateOne({user: req.body._id}, userProfile);
+            return res.status(200).send({message: "Your profile updated successfully!"});
+        } else {
+            profile = new Profile(userProfile);
+            await Profile.save();
+            return res.status(200).send({message: "Your profile info saved successfully!"});
+        }
+    } catch (error) {
+        return res.status(400).send({message: "An error occured."});
     }
 }
