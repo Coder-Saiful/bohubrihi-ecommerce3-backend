e8336b60192270a82c9aafc39356571c86e6f1cf
Md. Saiful Islam <80346558+Coder-Saiful@users.noreply.github.com>
2023-03-15 22:27:15 -0600

    Update profileController.js

--- controllers/profileController.js
+++ controllers/profileController.js
@@ -4,8 +4,10 @@ const _ = require('lodash');
 // get profile info
 module.exports.getProfile = async (req, res) => {
     try {
-        const prifle = await Profile.findOne({user: req.user._id});
-        return res.status(200).send(prifle);
+        const profile = await Profile.findOne({user: req.user._id})
+            .populate('user', 'name');
+        console.log(profile)
+        return res.status(200).send(profile);
     } catch (error) {
         return res.status(400).send({message: "An error occured."});
     }
@@ -16,9 +18,11 @@ module.exports.setProfile = async (req, res) => {
     try {
         const userProfile = _.pick(req.body, ["phone", "address1", "address2", "city", "state", "postcode", "country"]);
         userProfile.user = req.user._id;
+        userProfile.phone = parseInt(userProfile.phone);
+        userProfile.postcode = parseInt(userProfile.postcode);
         let profile = await Profile.findOne({user: req.user._id});
         if (profile) {
-            await Profile.updateOne({user: req.body._id}, userProfile);
+            await Profile.updateOne({user: req.user._id}, userProfile);
             return res.status(200).send({message: "Your profile updated successfully!"});
         } else {
             profile = new Profile(userProfile);
@@ -26,7 +30,7 @@ module.exports.setProfile = async (req, res) => {
             return res.status(201).send({message: "Your profile info saved successfully!"});
         }
     } catch (error) {
-        console.log(error);
+        console.log(error)
         return res.status(400).send({message: "An error occured."});
     }
 }
