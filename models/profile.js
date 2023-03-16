748d40624e2755ff4b6d4a4b9b703a0d115915f9
Md. Saiful Islam <80346558+Coder-Saiful@users.noreply.github.com>
2023-03-15 20:50:28 -0600

    Update profile.js

--- models/profile.js
+++ models/profile.js
@@ -1,31 +1,17 @@
-const {Profile} = require('../models/profile');
-const _ = require('lodash');
+const {Schema, model} = require('mongoose');
 
-// get profile info
-module.exports.getProfile = async (req, res) => {
-    try {
-        const prifle = await Profile.findOne({user: req.user._id});
-        return res.status(200).send(prifle);
-    } catch (error) {
-        return res.status(400).send({message: "An error occured."});
-    }
-}
-
-// set profile info
-module.exports.setProfile = async (req, res) => {
-    try {
-        const userProfile = _.pick(req.body, ["phone", "address1", "address2", "city", "state", "postcode", "country"]);
-        userProfile.user = req.user._id;
-        let profile = await Profile.findOne({user: req.user._id});
-        if (profile) {
-            await Profile.updateOne({user: req.body._id}, userProfile);
-            return res.status(200).send({message: "Your profile updated successfully!"});
-        } else {
-            profile = new Profile(userProfile);
-            await profile.save();
-            return res.status(201).send({message: "Your profile info saved successfully!"});
-        }
-    } catch (error) {
-        return res.status(400).send({message: "An error occured."});
-    }
-}
+module.exports.Profile = model('Profile', Schema({
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+        unique: true
+    },
+    phone: Number,
+    address1: String,
+    address2: String,
+    city: String,
+    state: String,
+    postcode: Number,
+    country: String
+}, {timestamps: true}));
