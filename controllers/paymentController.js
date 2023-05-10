45a0cc32f0a44ad8abec1ab461343440548c0bf2
Md. Saiful Islam <80346558+Coder-Saiful@users.noreply.github.com>
2023-05-10 12:09:16 -0600

    Update paymentController.js

--- controllers/paymentController.js
+++ controllers/paymentController.js
@@ -30,7 +30,7 @@ module.exports.initPayment = async (req, res) => {
         success: "yoursite.com/success", // If payment Succeed
         fail: "yoursite.com/fail", // If payment failed
         cancel: "yoursite.com/cancel", // If user cancel payment
-        ipn: "yoursite.com/ipn", // SSLCommerz will send http post request in this link
+        ipn: "https://bohubrihi-ecommerce3-backend.onrender.com/api/payment/ipn", // SSLCommerz will send http post request in this link
     });
 
     // Set order details
