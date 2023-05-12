72052632d574045bb59d66f39d67ab30fa1a1dd4
Md. Saiful Islam <80346558+Coder-Saiful@users.noreply.github.com>
2023-05-12 00:06:00 -0600

    Update index.js

--- middlewares/index.js
+++ middlewares/index.js
@@ -4,7 +4,11 @@ const morgan = require('morgan');
 const compression = require('compression');
 
 module.exports = (app) => {
-    app.use(cors());
+    app.use(cors({
+        origin: process.env.LOCAL_URL,
+        optionsSuccessStatus: 200,
+        methods: ["GET", "POST", "PUT", "DELETE"]
+    }));
     app.use(compression());
     app.use(express.json());
     app.use(express.urlencoded({extended: true}));
