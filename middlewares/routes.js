493cc2d4e0adfaa76fe2e1504fa3824e3d078d82
Md. Saiful Islam <80346558+Coder-Saiful@users.noreply.github.com>
2023-05-16 11:12:59 -0600

    Update routes.js

--- middlewares/routes.js
+++ middlewares/routes.js
@@ -12,7 +12,7 @@ module.exports = (app) => {
         res.setHeader("Access-Control-Max-Age", "1800");
         res.setHeader("Access-Control-Allow-Headers", "content-type");
         res.setHeader( "Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, PATCH, OPTIONS" ); 
-        res.send('<h2>Server running...</h2>');
+        res.send(`<h2>Server running...</h2><h3>${req.url}</h3>`);
     });
     app.use('/api/user', userRouter);
     app.use('/api/category', categoryRouter);
